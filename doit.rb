44860c2a33de6556161d311a228484ca6db1ff5a
Matt Spencer <sirmspencer@gmail.com>
2024-09-21 15:30:35 -0400

    I think sorta ready now

--- doit.rb
+++ doit.rb
@@ -1,8 +1,8 @@
 class Doit < Formula
   desc "a purposely simple aliasing tool"
   homepage ""
-  url "https://github.com/sirmspencer/do/archive/refs/tags/0.1.15.tar.gz"
-  sha256 "4d4f76b7aa46ea81086bf4a4ed6ed1bd7976a88bb3a71d8daf60963224bcd1f0"
+  url "https://github.com/sirmspencer/do/archive/refs/tags/0.1.17.tar.gz"
+  sha256 "4553a4de3cfeb99107f7de4bf94c85823523df3ae84547c34d78efc759db0dd7"
   license "GPL-3.0"
 
   depends_on "borkdude/brew/babashka"
