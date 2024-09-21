a87c80264b1c690d604f8fce02dba93a5e4b5475
Matt Spencer <sirmspencer@gmail.com>
2024-09-21 15:50:06 -0400

    No need to throw please (known errors)

--- doit.rb
+++ doit.rb
@@ -1,8 +1,8 @@
 class Doit < Formula
   desc "a purposely simple aliasing tool"
   homepage ""
-  url "https://github.com/sirmspencer/do/archive/refs/tags/0.1.17.tar.gz"
-  sha256 "4553a4de3cfeb99107f7de4bf94c85823523df3ae84547c34d78efc759db0dd7"
+  url "https://github.com/sirmspencer/doit/archive/refs/tags/v0.1.22.tar.gz"
+  sha256 "5f5499d1b0987981773a30a89e458936841113c86806c8dace4c2125e09c89a1"
   license "GPL-3.0"
 
   depends_on "borkdude/brew/babashka"
