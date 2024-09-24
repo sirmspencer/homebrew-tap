3b640189fc6a0430c653f8ec66aeb0bd0733139e
Matt Spencer <sirmspencer@gmail.com>
2024-09-23 20:17:26 -0400

    auto-publish

--- doit.rb
+++ doit.rb
@@ -1,13 +1,14 @@
 class Doit < Formula
   desc "a purposely simple aliasing tool"
   homepage ""
-  url "https://github.com/sirmspencer/doit/archive/refs/tags/v0.1.22.tar.gz"
-  sha256 "5f5499d1b0987981773a30a89e458936841113c86806c8dace4c2125e09c89a1"
+  url "https://github.com/sirmspencer/doit/archive/refs/tags/v0.1.29.tar.gz"
+  sha256 ""
   license "GPL-3.0"
 
   depends_on "borkdude/brew/babashka"
 
   def install
     bin.install "bb/doit"
+    bash_completion.install "doit-completions.bash"
   end
 end
